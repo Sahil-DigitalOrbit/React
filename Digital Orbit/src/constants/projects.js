51708d2e8cca610522d56442dd298aa3f6852722
Sahil-DigitalOrbit <sahil.singh@digitalorbit.online>
2024-12-06 12:43:28 +0530

    Initial commit

--- Digital Orbit/src/constants/projects.js
+++ Digital Orbit/src/constants/projects.js
@@ -1,32 +1,32 @@
 export const projects = [
   {
-    imgUrl: "../src/assets/shoe.png",
+    imgUrl: "./assets/shoe.png",
     title: "Shoe Store App",
     disclaimer: "Maximise your Brand's Reach & Impact with Digital Marketing",
   },
   {
-    imgUrl: "../src/assets/gym.jpg",
+    imgUrl: "./assets/gym.jpg",
     title: "Fitness App",
     disclaimer:
       "Grow your business 5x with custom apps that boost engagement and efficiency",
   },
   {
-    imgUrl: "../src/assets/weed.jpg",
+    imgUrl: "./assets/weed.jpg",
     title: "Cannabis Delivery App",
     disclaimer: "Protect your Online Business with Advanced Security Measures",
   },
   {
-    imgUrl: "../src/assets/shop.jpg",
+    imgUrl: "./assets/shop.jpg",
     title: "POS Application",
     disclaimer: "Protect your Online Business with Advanced Security Measures",
   },
   {
-    imgUrl: "../src/assets/book.png",
+    imgUrl: "./assets/book.png",
     title: "E-Learning App",
     disclaimer: "Protect your Online Business with Advanced Security Measures",
   },
   {
-    imgUrl: "../src/assets/message.png",
+    imgUrl: "./assets/message.png",
     title: "E-Learning App",
     disclaimer: "Protect your Online Business with Advanced Security Measures",
   },
